aa67d3bc11b1a3ef2e992560ba25d6abe45a181b
stanli124 <1353276354@qq.com>
2021-12-07 22:27:18 +0800

    增加了两数之和的解法

--- leecode/simple/src/sum_of_twonums.java
+++ leecode/simple/src/sum_of_twonums.java
@@ -12,14 +12,14 @@
 
 
 
-
+import java.util.Arrays;
 
 
 public class sum_of_twonums {
 
     public static void main(String[] args) {
-        int[] a = new int[]{3,2,95,4,-3};
-        int[] b = twoSum(a,92);
+        int[] a = new int[]{3,2,4};
+        int[] b = twoSum(a,6);
         for (int s : b){
             System.out.println(s);
         }
@@ -29,7 +29,7 @@ public class sum_of_twonums {
         int[] index = new int[2];
         int len_nums = nums.length;
 
-        //没有解决数组同时包含正数、负数的情况
+        //没有解决数组同时包含正数、负数的情况; 这种没有通过所有案例
 //        for (int i = 0; i < len_nums; i++) {
 //            if ((target > 0) &&(nums[i] > target)) continue;
 //
@@ -45,17 +45,37 @@ public class sum_of_twonums {
 //            }
 //        }
 
+        //两层for循环，暴力破解；从第一个数字开始，依次与其后的数字进行匹配，若==target,则找到两个值；若没有，则依次对后面的数字执行上述步骤
+//        for (int i = 0; i < len_nums; i++) {
+//                for (int j = i + 1; j < len_nums; j++) {
+//                    if (nums[i] + nums[j] == target) {
+//                        index[0] = i;
+//                        index[1] = j;
+//                        return index;
+//                    }
+//                }
+//        }
 
-        for (int i = 0; i < len_nums; i++) {
-                for (int j = i + 1; j < len_nums; j++) {
-                    if (nums[i] + nums[j] == target) {
-                        index[0] = i;
-                        index[1] = j;
-                        return index;
-                    }
-                }
+        //先排序，在找
+        int[] nums2 = Arrays.copyOf(nums,len_nums);
+        Arrays.sort(nums);
+        int i=0,j=len_nums-1;
+
+        while ((i <= len_nums-1) && (nums[i] + nums[j] != target)){
+            if(nums[i] + nums[j] < target){i++;}
+            else {j--;}
         }
 
+        int flag = 0;
+        int k = 0;
+
+        while (k <= len_nums-1 && flag != 2){
+            if (nums[i] == nums2[k] | nums[j] == nums2[k]){
+                index[flag] = k;
+                flag++;
+            }
+            k++;
+        }
 
         return index;
     }
