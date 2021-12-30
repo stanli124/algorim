11e9f03808ed1dd2ae1c9da79d3a7211b21cd0c9
stanli124 <1353276354@qq.com>
2021-12-30 22:06:58 +0800

    问题67新增解法

--- leecode/Tag/src/zifuchuan/problem67.java
+++ leecode/Tag/src/zifuchuan/problem67.java
@@ -142,36 +142,62 @@ public class problem67 {
 //        if (jinWei=='1') binarySum.insert(0, '1');
 //        return binarySum.toString();
 
+
+
+
+        //其他人写的
+//        int i = a.length() - 1;
+//        int j = b.length() - 1;
+//        int carry = 0;
+//        StringBuilder builder = new StringBuilder();
+//        //循环相加两个字符串相同长度的低位数部分
+//        while (i >= 0 && j >= 0) {
+//            int sum = carry;
+//            sum += a.charAt(i--) - '0';
+//            sum += b.charAt(j--) - '0';
+//            carry = sum / 2;
+//            builder.append(sum % 2);
+//        }
+//        // 如果 a 还没遍历完成（a串比b串长），则继续遍历添加 a 的剩余部分
+//        while (i >= 0) {
+//            int sum = carry + a.charAt(i--) - '0';
+//            carry = sum / 2;
+//            builder.append(sum % 2);
+//        }
+//        // 如果 b 还没遍历完成（b串比a串长），则继续遍历添加 b 的剩余部分
+//        while (j >= 0) {
+//            int sum = carry + b.charAt(j--) - '0';
+//            carry = sum / 2;
+//            builder.append(sum % 2);
+//        }
+//        //如果 carry 不等于0 还有个进位数没加进去，需要补充
+//        if (carry == 1) {
+//            builder.append(carry);
+//        }
+//        //反转字符串获得正常结果
+//        return builder.reverse().toString();
+
+
+
+        //其他人写的简化版本
+        if(a == null || a.length() == 0) return b;
+        if(b == null || b.length() == 0) return a;
+
+        StringBuilder stb = new StringBuilder();
         int i = a.length() - 1;
         int j = b.length() - 1;
-        int carry = 0;
-        StringBuilder builder = new StringBuilder();
-        //循环相加两个字符串相同长度的低位数部分
-        while (i >= 0 && j >= 0) {
-            int sum = carry;
-            sum += a.charAt(i--) - '0';
-            sum += b.charAt(j--) - '0';
-            carry = sum / 2;
-            builder.append(sum % 2);
+
+        int c = 0;  // 进位
+        while(i >= 0 || j >= 0) {
+            if(i >= 0) c += a.charAt(i --) - '0';
+            if(j >= 0) c += b.charAt(j --) - '0';
+            stb.append(c % 2);
+            c >>= 1;
         }
-        // 如果 a 还没遍历完成（a串比b串长），则继续遍历添加 a 的剩余部分
-        while (i >= 0) {
-            int sum = carry + a.charAt(i--) - '0';
-            carry = sum / 2;
-            builder.append(sum % 2);
-        }
-        // 如果 b 还没遍历完成（b串比a串长），则继续遍历添加 b 的剩余部分
-        while (j >= 0) {
-            int sum = carry + b.charAt(j--) - '0';
-            carry = sum / 2;
-            builder.append(sum % 2);
-        }
-        //如果 carry 不等于0 还有个进位数没加进去，需要补充
-        if (carry == 1) {
-            builder.append(carry);
-        }
-        //反转字符串获得正常结果
-        return builder.reverse().toString();
+
+        String res = stb.reverse().toString();
+        return c > 0 ? '1' + res : res;
+
 
     }
 
