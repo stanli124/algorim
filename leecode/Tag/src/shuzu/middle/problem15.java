d13a18dd1a5339533de5869722230d459892979a
stanli124 <1353276354@qq.com>
2022-01-06 12:07:11 +0800

    问题15

--- leecode/Tag/src/shuzu/middle/problem15.java
+++ leecode/Tag/src/shuzu/middle/problem15.java
@@ -5,5 +5,19 @@
 
 package shuzu.middle;
 
+import java.util.List;
+
 public class problem15 {
+
+    public static void main(String[] args) {
+
+        System.out.println(threeSum(new int[]{-1,0,1,2,-1,-4}));
+
+    }
+
+    public static List<List<Integer>> threeSum(int[] nums) {
+
+        return ;
+    }
+
 }
