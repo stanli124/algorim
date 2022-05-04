cb6f290359cdb95f827a008bc8af0c42edc673a7
stanli124 <1353276354@qq.com>
2022-05-04 18:31:26 +0800

    hot100

---
+++ leecode/Tag/src/hot100/middle/problem406.java
@@ -0,0 +1,19 @@
+/**
+ *  输入people，
+ */
+
+package hot100.middle;
+
+public class problem406 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int[][] reconstructQueue(int[][] people) {
+
+
+        return new int[][]{};
+    }
+
+}
