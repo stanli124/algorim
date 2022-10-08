ce574d23a4d59d19d0d887e455ba0ab9fe33e5a1
stanli124 <1353276354@qq.com>
2022-10-08 11:42:50 +0800

    剑指offer面试题59

--- leecode/Tag/src/JianZhiOffer/Interview59.java
+++ leecode/Tag/src/JianZhiOffer/Interview59.java
@@ -1,8 +1,38 @@
 package JianZhiOffer;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 //队列的最大值
 public class Interview59 {
 
+    static Queue<Integer> q = new LinkedList<>();
 
+    public static void main(String[] args) {
+        q.isEmpty();
+    }
+}
+class MaxQueue {
+    Queue<Integer> q;
+    public MaxQueue() {
+        q = new LinkedList<>();
+    }
 
+    public int max_value() {
+        if (q.isEmpty()) return -1;
+        int max = -1;
+        for(int i : q){
+            if (i > max) max = i;
+        }
+        return max;
+    }
+
+    public void push_back(int value) {
+        q.offer(value);
+    }
+
+    public int pop_front() {
+        if (q.isEmpty()) return -1;
+        return q.poll();
+    }
 }
