72c210f22bf6f41de801d77bc155827d4fcafbd3
stanli124 <1353276354@qq.com>
2023-06-08 10:43:35 +0800

    leetcode231题

--- leecode/simple/src/PowerOfTwo.java
+++ leecode/simple/src/PowerOfTwo.java
@@ -5,7 +5,7 @@ public class PowerOfTwo {
         public boolean isPowerTwo(int n){
             int num = 1;
             for (int i = 1; i < 32; i++) {
-                if (num == n) return true;
+                if (num == n) return true; //如果将条件i<32设置为i<31，那么在i==30<31的时候会判断为true进入循环num变为1073741824，但这个时候i加1为31，那么下一次就不会进入循环，也就不会返回true
                 num = num * 2;
             }
             return false;
